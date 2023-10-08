44a24e6ab6d6c6ba3ec79bf8358f2317a7ba6972
Omar Hernandez <omarsro@gmail.com>
2023-10-08 18:31:17 -0500

    feat(constants.js): dynamically set BOARD_WIDTH and BOARD_HEIGHT based on isMobile condition to improve responsiveness on mobile devices
    feat(index.html): add meta viewport tag to ensure proper scaling on mobile devices
    
    style(index.html): adjust width and height of #app to ensure proper display on different screen sizes
    
    feat(package.json): add 'host' script to allow specifying a custom host for the development server
    
    style(style.css): adjust various styles to improve responsiveness and reduce size on mobile devices

--- constants.js
+++ constants.js
@@ -1,3 +1,4 @@
+export const isMobile = window.innerWidth < 768;
 export const BLOCK_SIZE = 20;
-export const BOARD_WIDTH = 15;
-export const BOARD_HEIGHT = 30;
+export const BOARD_WIDTH = isMobile ? 10 : 15;
+export const BOARD_HEIGHT = isMobile ? 20 : 30;
