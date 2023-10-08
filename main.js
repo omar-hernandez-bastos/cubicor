f39f54b39f3832fd1592c651b247b14273776cc0
Omar Hernandez <omarsro@gmail.com>
2023-10-08 10:35:21 -0500

    feat: add constants for block size, board width, and board height
    The constants `BLOCK_SIZE`, `BOARD_WIDTH`, and `BOARD_HEIGHT` have been added to the `constants.js` file. These constants define the size of each block in the game, the width of the game board, and the height of the game board, respectively. These constants will be used throughout the codebase to ensure consistency and improve readability.
    
    chore: update favicon and site manifest
    
    The favicon and site manifest have been updated in the `index.html` file. The new favicon files have been added and the links to the favicon files have been updated. The site manifest file has also been updated to include the new favicon files.
    
    chore: update game logo and styling
    
    The game logo and styling have been updated in the `index.html` and `style.css` files. The game logo has been replaced with a new logo image file. The styling of the game elements, such as the canvas, score container, and next piece container, has been modified to improve the visual appearance of the game.
    
    fix: change port variable case from lowercase port to uppercase PORT
    
    The variable `port` in the `server.ts` file has been changed to `PORT` to improve semantics and follow naming conventions. This change ensures consistency and clarity in the codebase.
    
    feat: add support for process.env.PORT environment variable
    
    The `app.listen` function in the `server.ts` file has been updated to support the `process.env.PORT` environment variable. This change allows the app to be run on a configurable port specified by the environment variable. This improves flexibility and makes it easier to deploy the app in different environments.

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 import "./style.css";
-
-// Init Canvas
+import { BOARD_WIDTH, BOARD_HEIGHT, BLOCK_SIZE } from "./constants.js";
 
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
-
-const BLOCK_SIZE = 20;
-const BOARD_WIDTH = 14;
-const BOARD_HEIGHT = 30;
+let gameOver = false;
 let score = 0;
 
 canvas.width = BLOCK_SIZE * BOARD_WIDTH;
@@ -15,97 +11,83 @@ canvas.height = BLOCK_SIZE * BOARD_HEIGHT;
 
 context.scale(BLOCK_SIZE, BLOCK_SIZE);
 
-// board
-const board = [
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
-// piece player
-let piece = {
-  position: { x: 5, y: 5 },
-  shape: [
-    [1, 1],
-    [1, 1],
-  ],
-  color: "yellow",
-  name: "Square",
+const createEmptyBoard = (width, height) => {
+  const board = [];
+  for (let i = 0; i < height; i++) {
+    board.push(Array(width).fill(0));
+  }
+  return board;
 };
+
+const board = createEmptyBoard(BOARD_WIDTH, BOARD_HEIGHT);
 
 // Random pieces
 const PIECES = [
   {
-    position: { x: 5, y: 5 },
+    position: { x: 5, y: 0 },
     shape: [
       [1, 1],
       [1, 1],
     ],
-    color: "yellow",
+    color: "purple",
     name: "Square",
   },
   {
-    position: { x: 5, y: 5 },
+    position: { x: 5, y: 0 },
     shape: [[1, 1, 1, 1]],
-    color: "cyan",
+    color: "green",
     name: "Line",
   },
   {
-    position: { x: 5, y: 5 },
+    position: { x: 5, y: 0 },
     shape: [
       [0, 1, 0],
       [1, 1, 1],
     ],
-    color: "blue",
+    color: "#66ccff",
     name: "T-shape",
   },
   {
-    position: { x: 5, y: 5 },
+    position: { x: 5, y: 0 },
+    shape: [
+      [0, 1, 1],
+      [1, 1, 0],
+    ],
+    color: "#FFDF40",
+    name: "Z-shape-reverse",
+  },
+  {
+    position: { x: 5, y: 0 },
     shape: [
       [1, 1, 0],
       [0, 1, 1],
     ],
-    color: "purple",
-    name: "L-shape",
+    color: "#FFDF40",
+    name: "Z-shape",
   },
   {
-    position: { x: 5, y: 5 },
+    position: { x: 5, y: 0 },
     shape: [
       [1, 0],
       [1, 0],
       [1, 1],
     ],
-    color: "orange",
+    color: "#FF5959",
     name: "J-shape",
   },
+  {
+    position: { x: 5, y: 0 },
+    shape: [
+      [0, 1],
+      [0, 1],
+      [1, 1],
+    ],
+    color: "#FF5959",
+    name: "J-shape-reverse",
+  },
 ];
+
+let piece = PIECES[Math.floor(Math.random() * PIECES.length)];
 
 // Game loop
 let dropCounter = 0;
@@ -118,10 +100,45 @@ function update(time = 0) {
     movePiece("y", 1);
     dropCounter = 0;
   }
-
+  drawNextPiece();
   draw();
-  requestAnimationFrame(update);
+
+  if (!gameOver) requestAnimationFrame(update);
 }
+
+const nextCanvas = document.querySelector("#next-piece");
+const nextContext = nextCanvas.getContext("2d");
+nextCanvas.width = BLOCK_SIZE * 4;
+nextCanvas.height = BLOCK_SIZE * 4;
+let nextPiece = PIECES[Math.floor(Math.random() * PIECES.length)];
+function drawNextPiece() {
+  const blockSize = 4; // El tetromino más grande es de 4 bloques
+  const scale = nextCanvas.width / blockSize;
+
+  nextContext.scale(scale, scale); // Aplicamos la escala
+  nextContext.fillStyle = "#000";
+  nextContext.fillRect(0, 0, blockSize, blockSize); // Llenamos con base en la escala
+
+  // Calcula el offset para centrar la pieza
+  const offsetX = (blockSize - nextPiece.shape[0].length) / 2;
+  const offsetY = (blockSize - nextPiece.shape.length) / 2;
+
+  nextPiece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value > 0) {
+        nextContext.fillStyle = nextPiece.color;
+        nextContext.fillRect(x + offsetX, y + offsetY, 1, 1); // Dibujamos con base en la escala y el offset
+
+        nextContext.strokeStyle = "black";
+        nextContext.lineWidth = 0.05; // Ajustamos el ancho de la línea según la escala
+        nextContext.strokeRect(x + offsetX, y + offsetY, 1, 1); // Dibujamos con base en la escala y el offset
+      }
+    });
+  });
+
+  nextContext.setTransform(1, 0, 0, 1, 0, 0); // Restablecemos la transformación para la próxima vez
+}
+
 function draw() {
   context.fillStyle = "#000";
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -166,7 +183,7 @@ function draw() {
 }
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowUp") {
+  if (event.code === "Space") {
     const rotated = [];
     for (let i = 0; i < piece.shape[0].length; i++) {
       const row = [];
@@ -191,45 +208,86 @@ document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowDown") {
     movePiece("y", 1);
   }
+  if (event.key === "ArrowUp") {
+    dropPieceToBottom();
+  }
 });
-
-function movePiece(axis, direction) {
+function isInsideBounds(axis, direction) {
+  const newPosition = piece.position[axis] + direction;
+  if (axis === "x") {
+    return (
+      newPosition >= 0 && newPosition + piece.shape[0].length <= BOARD_WIDTH
+    );
+  } else if (axis === "y") {
+    return newPosition >= 0 && newPosition + piece.shape.length <= BOARD_HEIGHT;
+  }
+  return false;
+}
+function moveAndCheckCollision(axis, direction) {
   piece.position[axis] += direction;
   if (checkCollision()) {
     piece.position[axis] -= direction;
+    return true; // There was a collision
+  }
+  return false; // No collision
+}
+function movePiece(axis, direction) {
+  if (isInsideBounds(axis, direction)) {
+    const collided = moveAndCheckCollision(axis, direction);
+
+    if (axis === "y" && collided) {
+      solidifyPiece();
+      removeRows();
+    }
+  } else if (axis === "y") {
     solidifyPiece();
     removeRows();
   }
 }
-
 function checkCollision() {
-  return piece.shape.find((row, y) => {
-    return row.find((value, x) => {
-      return (
-        value === 1 && board[y + piece.position.y]?.[x + piece.position.x] !== 0
-      );
-    });
-  });
+  for (let y = 0; y < piece.shape.length; y++) {
+    for (let x = 0; x < piece.shape[y].length; x++) {
+      if (
+        piece.shape[y][x] === 1 &&
+        board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      ) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 function solidifyPiece() {
   piece.shape.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value === 1) {
-        board[y + piece.position.y][x + piece.position.x] = 1;
+        const boardY = y + piece.position.y;
+        const boardX = x + piece.position.x;
+        if (board[boardY] && board[boardY][boardX] !== undefined) {
+          // Comprueba si la posición es válida antes de intentar acceder
+          board[boardY][boardX] = 1;
+        } else {
+          // Esto puede ser un buen lugar para invocar el game over si una pieza se solidifica fuera de los límites
+          showGameOver();
+          return;
+        }
       }
     });
   });
-  // reset
+
+  piece = nextPiece;
   piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2);
   piece.position.y = 0;
-  // get random shape
-  piece = PIECES[Math.floor(Math.random() * PIECES.length)];
-  //gameover
+
+  nextPiece = PIECES[Math.floor(Math.random() * PIECES.length)];
+  drawNextPiece();
+
   if (checkCollision()) {
-    alert("Game Over =(");
+    showGameOver();
     board.forEach((row) => row.fill(0));
   }
 }
+
 function removeRows() {
   const rowsToRemove = [];
   board.forEach((row, y) => {
@@ -237,14 +295,97 @@ function removeRows() {
       rowsToRemove.push(y);
     }
   });
-  rowsToRemove.forEach((y) => {
-    board.splice(y, 1);
-    const newRow = Array(BOARD_WIDTH).fill(0);
-    board.unshift(newRow);
-    score += 10;
+
+  // Si hay filas para eliminar, hagámoslas parpadear
+  if (rowsToRemove.length > 0) {
+    let flashCount = 0;
+    const maxFlashes = 3;
+    const flashSpeed = 150; // en milisegundos
+
+    const flashInterval = setInterval(() => {
+      rowsToRemove.forEach((y) => {
+        board[y].forEach((value, x) => {
+          board[y][x] = flashCount % 2 === 0 ? 0 : 1;
+        });
+      });
+      draw(); // Redibujamos el tablero con el parpadeo
+      flashCount++;
+
+      if (flashCount > maxFlashes * 2 - 1) {
+        // Terminamos después de 3 parpadeos (encendido y apagado)
+        clearInterval(flashInterval);
+        // Luego de parpadear, eliminamos las filas
+        rowsToRemove.forEach((y) => {
+          board.splice(y, 1);
+          const newRow = Array(BOARD_WIDTH).fill(0);
+          board.unshift(newRow);
+          score += 10;
+        });
+      }
+    }, flashSpeed);
+  }
+}
+function dropPieceToBottom() {
+  while (!checkCollision()) {
+    piece.position.y += 1;
+  }
+  piece.position.y -= 1;
+
+  // Solidify the piece without checking for collision again
+  piece.shape.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value === 1) {
+        const boardY = y + piece.position.y;
+        const boardX = x + piece.position.x;
+        if (board[boardY] && board[boardY][boardX] !== undefined) {
+          board[boardY][boardX] = 1;
+        } else {
+          showGameOver();
+          return;
+        }
+      }
+    });
   });
+
+  piece = nextPiece;
+  piece.position.x = Math.floor(BOARD_WIDTH / 2 - 2);
+  piece.position.y = 0;
+
+  nextPiece = PIECES[Math.floor(Math.random() * PIECES.length)];
+  drawNextPiece();
+
+  if (checkCollision()) {
+    showGameOver();
+    board.forEach((row) => row.fill(0));
+  }
 }
 
 update();
 
 // auto drop
+function showGameOver() {
+  gameOver = true;
+
+  // Muestra el div de Game Over y establece el puntaje final
+  const gameOverScreen = document.querySelector("#gameOverScreen");
+  const finalScoreElement = document.querySelector("#finalScore");
+
+  finalScoreElement.textContent = `Score: ${score}`;
+  gameOverScreen.style.display = "block";
+}
+document.querySelector("#restartButton").addEventListener("click", () => {
+  const gameOverScreen = document.querySelector("#gameOverScreen");
+  gameOverScreen.style.display = "none";
+
+  // Reiniciar juego
+  board.forEach((row) => row.fill(0));
+  score = 0;
+  piece = PIECES[Math.floor(Math.random() * PIECES.length)];
+  piece.position.y = 0;
+  nextPiece = PIECES[Math.floor(Math.random() * PIECES.length)];
+  drawNextPiece();
+  gameOver = false; // Esto es importante para reiniciar el estado del juego
+  draw();
+
+  update(); // ¡No olvides reiniciar el bucle del juego aquí!
+});
